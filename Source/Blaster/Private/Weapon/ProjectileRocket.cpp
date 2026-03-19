60bc1ec973bb7981b3e18774546483987f5848e9
28959 <28959@example.com>
2026-03-19 12:48:23 +0800

    Rocket Projectiles

---
+++ Source/Blaster/Private/Weapon/ProjectileRocket.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/ProjectileRocket.h"
+
+#include "Kismet/GameplayStatics.h"
+
+AProjectileRocket::AProjectileRocket()
+{
+	RocketMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RocketMesh"));
+	RocketMesh->SetupAttachment(RootComponent);
+	RocketMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AProjectileRocket::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (APawn* FiringPawn = GetInstigator())
+	{
+		if (AController* FiringController = FiringPawn->GetController())
+		{
+			UGameplayStatics::ApplyRadialDamageWithFalloff(
+				this,
+				BaseDamage,
+				10.f,
+				GetActorLocation(),
+				DamageRadius * 0.5f,
+				DamageRadius,
+				1.f,
+				UDamageType::StaticClass(),
+				TArray<AActor*>(),
+				this,
+				FiringController
+			);
+		}
+	}
+	Super::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+}
